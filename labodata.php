1536cf05dcb839900eab70cfa540991dc1b670f3
Benoit Asselin <benoit@161.io>
2017-05-17 11:40:10 +0200

    0.3.1

--- labodata.php
+++ labodata.php
@@ -34,7 +34,7 @@ class LaboData extends Module
     {
         $this->name = 'labodata';
         $this->tab = 'others';
-        $this->version = '0.3.0';
+        $this->version = '0.3.1';
         $this->author = '161 SARL';
 
         // 0 = Front // 1 = Back-office
